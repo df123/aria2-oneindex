818f451af183d2d3ae3bc6fde8f12024746ac2ca
df123 <hws184433274hws@live.com>
2020-03-29 13:29:48 +0800

    修改aria2安装

--- aria2.sh
+++ aria2.sh
@@ -120,7 +120,7 @@ on-download-complete=$user_path/.aria2/automatic_move.sh
 on-download-stop=$user_path/.aria2/automatic_delete.sh
 allow-overwrite=true" > $user_path/.aria2/aria2.conf
 
-    work_path=`pwd`
+    work_path=`dirname $0`
     mv $work_path/aria2.service /etc/systemd/system/
     systemctl enable aria2.service
     systemctl start aria2.service
